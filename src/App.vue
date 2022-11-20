225f4b6112c17a1db4a8065bfcf70c6f7dcc693b
jiwidi <fhjaime96@gmail.com>
2022-11-20 01:47:20 +0100

    style update

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,37 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="left">
+    <h1>Lens Database</h1>
+  </div>
+  <div class="container">
 
-    <v-table :data="lenses">
- <template #head>
-   <thead slot="head">
-     <th>Name</th>
-     <th>mount</th>
-   </thead>
- </template>
- <template #body="{ rows }">
-   <tbody>
-     <tr v-for="row in rows" :key="row.name">
-       <td>{{ row.name }}</td>
-       <td>{{ row.mount }}</td>
-     </tr>
-   </tbody>
- </template>
-</v-table>
-  </template>
+  <VTable :data="lenses">
+    <template #head>
+      <tr>
+        <th>Name</th>
+        <th>Mount</th>
+      </tr>
+    </template>
+    <template #body="{rows}">
+      <tr v-for="row in rows" :key="row.name">
+        <td>{{ row.name }}</td>
+        <td>{{ row.mount }}</td>
+      </tr>
+    </template>
+  </VTable>
+  </div>
+</template>
 
 <script>
-import lenses from './data/lenses.json'
+import lenses from "./data/lenses.json";
 
 export default {
-    name: 'main',
-    data: () => ({
-        lenses
-    })
-}
+  name: "main",
+  data: () => ({
+    lenses,
+  }),
+};
 </script>
 
-
-<style scoped>
-header {
-    line-height: 1.5;
-}
-
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-}
-</style>
+<style scoped></style>
