f4e174998ffc0d365090e8d1072581b5d1e3b0e4
lecarles <carles2612@gmail.com>
2022-11-23 00:33:11 +0100

    Add toggeable aperture filter

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,18 @@
 <template>
   <div class="app-wrapper">
     <div class="sidebar">
-      <h1>Lens Database filter sidebar</h1>
-      <label>Filter by Name:</label>
+      <h1>Lens Database</h1>
+      <h2>Filter by Name:</h2>
       <input class="form-control" v-model="filters.name.value" />
-      <button class="ui button toggle" @click="filters.type.value.push('Zoom')">Filter by Zoom</button>
-      <ToggleButton @click="pishita(filters, 'type', 'Zoom', $event.target)">Hello</ToggleButton>
+      <h2>Type</h2>
+      <div class="button-group">
+        <FilterButton @update-filter="(action) => pishita('type', 'Zoom', action)">Zoom</FilterButton>
+        <FilterButton @update-filter="(action) => pishita('type', 'Prime', action)">Prime</FilterButton>
+      </div>
+      <h2>Aperture</h2>
+      <div class="button-group">
+        <FilterButton v-for="aperture in apertures" :key="aperture" @update-filter="(action) => pishita('aperture', aperture, action)">{{ aperture }}</FilterButton>
+      </div>
     </div>
     <div class="content">
       <VTable :data="lenses" :filters="filters">
@@ -37,18 +44,24 @@
 
 <script>
 import lenses from './data/lenses.json'
-import ToggleButton from './components/ToggleButton.vue'
+import FilterButton from './components/FilterButton.vue'
 
 export default {
   name: "main",
   data() {
     return {
       lenses,
+      apertures: null,
       filters: {
         name: { value: "", keys: ["name"] },
-        type: { value: [], custom: this.typeFilter }
+        type: { value: [], custom: this.typeFilter },
+        aperture: { value: [], custom: this.apertureFilter }
       }
     }
+  },
+
+  mounted () {
+    this.apertures = [...new Set(this.lenses.map(lens => lens.maxAperture))]
   },
 
   methods: {
@@ -57,15 +70,18 @@ export default {
       else return filterValue.includes(row.type)
     },
 
-    pishita (filters, field, value, button) {
-      button.isActive = this.isActive ? false : true
-      console.log(button.isActive)
-      filters[field].value.push(value)
+    apertureFilter (filterValue, row) {
+      if (filterValue.length === 0) return true
+      else return filterValue.includes(row.maxAperture)
+    },
+
+    pishita (field, value, action) {
+      action === 'add' ? this.filters[field].value.push(value) : this.filters[field].value.pop(value)
     }
   },
 
   components: {
-    ToggleButton,
+    FilterButton,
   }
 }
 </script>
---
+++ src/components/FilterButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+let isActive = ref(false)
+
+const emit = defineEmits(['updateFilter'])
+
+function handleFilter() {
+  isActive.value = !isActive.value
+  emit('updateFilter', isActive.value ? 'add' : 'remove')
+}
+</script>
+
+<template>
+  <button :class="{ active : isActive }" @click="handleFilter">
+    <slot />
+  </button>
+</template>
